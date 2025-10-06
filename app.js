9e254e8190f6fbb76e69ce8c54701c435d126956
Formentini1 <formentini.silva@escola.pr.gov.br>
2025-10-05 22:44:44 -0300

    up

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const imagem =document.querySelectorAll('img')
+const imagens = document.querySelectorAll('img');
 let foto_troca = false;
 
-function trocarfotos(){
-
-if(foto_troca === false){
-    for( let i = 0; i < imagem.length; i++){
-        imagem[i].src = fut${i+8}.jpg;
-        foto_troca = true;}
-    }else{
-
-    for( let i = 0; i < imagem.length; i++){
-    imagem[i].src = fut${i+1}.jpg;
-    foto_troca = false;
-         } 
-    } }
+function trocarfotos() {
+  if (!foto_troca) {
+    for (let i = 0; i < imagens.length; i++) {
+      imagens[i].src = `fut${i + 8}.jpg`;
+    }
+    foto_troca = true;
+  } else {
+    for (let i = 0; i < imagens.length; i++) {
+      imagens[i].src = `fut${i + 1}.jpg`;
+    } 
+}
+}
